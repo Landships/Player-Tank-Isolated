60f85ef0a70abc5e0e4dfeabae91010e86b58bfe
RG <cnenrs@gmail.com>
2016-12-03 18:18:02 -0800

    added firing button

---
+++ Assets/Button.cs
@@ -0,0 +1,22 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+
+    public class Button : VRTK_InteractableObject
+    {
+        public float downForce;
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+            Debug.Log("pushed");
+            Debug.Log(this.gameObject);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(downForce, 0f, 0f), ForceMode.VelocityChange);
+
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+    }
+}
